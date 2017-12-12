309abdd0102c4b0950191481cf42051abc30d63a
ozgurgan <ozgurgan@gmail.com>
2017-12-12 23:49:51 +0300

    initial configuration

--- tracker.py
+++ tracker.py
@@ -1,27 +1,64 @@
 import requests
-import json
-import time
+import configparser
+import psycopg2
+import os
 
-while(True):
-    list = ['btcusd','ethusd','ethbtc','ltcusd','ltcbtc','iotusd','iotbtc']
-    ticker_list = []
-    my_amount = [0, 0, 0, 0, 0, 0, 0]
-    total = 0.0
-    n = 0
+config = configparser.ConfigParser()
 
-    for pair in list:
-        time.sleep(5)
-        url = "https://api.bitfinex.com/v1/pubticker/"+pair
+def get_cfg(string):
+    config.read('config.ini')
+    return config['DATABASE'][string]
+
+def sql_init():
+    try:
+        connect_str = "dbname='" + get_cfg('dbname') + "' user='" + get_cfg('user') + "' host='" + get_cfg('host') + "' " + \
+                      "password='" + get_cfg('password') + "'"
+        conn = psycopg2.connect(connect_str)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                SELECT COUNT(*)
+                FROM information_schema.tables
+                WHERE table_name = 'ticker'
+                """.format('ticker'.replace('\'', '\'\'')))
+        if cursor.fetchone()[0] == 0:
+            cursor.execute("""CREATE TABLE ticker (pair char(40), value real);""")
+            conn.commit()
+        # create a new table with columns "pair" and "value"
+        else:
+            print("Table already exists.")
+    except Exception as e:
+        print("Uh oh, can't connect. Have you checked dbname, user or password?")
+        print(e)
+        return 0
+
+def initial_cfg():
+    #config file
+    if (os.path.isfile('./config.ini') is not True):
+        config['DEFAULT'] = {'TickerUrl': 'https://api.bitfinex.com/v1/pubticker/',
+                          'RiskQuotient': '0.1'}
+        config['CURRENCIES'] = {'c1': 'btc',
+                             'c2': 'eth',
+                             'c3': 'ltc',
+                             'c4': 'iot'}
+        config['DATABASE'] = {'dbname': 'testpython',
+                           'user': 'portfopti',
+                           'host': 'localhost',
+                           'password': 'MYPASSWORD'}
+        with open('config.ini', 'w') as cfg_file:
+            config.write(cfg_file)
+    #sql db
+    sql_init()
+
+# ticker and data populator
+def ticker():
+    for key in config['CURRENCIES']:
+        url = config['DEFAULT']['TickerUrl'] + key + "usd"
         response = requests.request("GET", url)
         resp_json = response.text
         parsed_json = json.loads(resp_json)
-        ticker_list.append(parsed_json['mid'])
+        current_price = parsed_json['mid']
+    return 0
 
-    for value in ticker_list:
-        total += float(value) * my_amount[n]
-        n += 1
-
-    with open('my_money.csv', 'a') as myfile:
-        myfile.write(str(total)+'\n')
-        myfile.close()
-
+#MAIN
+initial_cfg()
