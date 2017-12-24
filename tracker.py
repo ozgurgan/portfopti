47d50d734ececa24692e7f893a53dd6f54e9fa16
ozgurgan <ozgurgan@gmail.com>
2017-12-24 20:09:22 +0300

    Changed console argument for pairs to provide flexibility

--- tracker.py
+++ tracker.py
@@ -2,12 +2,12 @@ import requests
 import argparse
 from time import sleep as sleep
 parser = argparse.ArgumentParser()
-parser.add_argument("arg_currency", help="Please enter a valid cryptocurrency name")
+parser.add_argument("arg_pair", help="Please enter a valid cryptocurrency pair like xrpusd")
 args = parser.parse_args()
 # Tick a single currency pair (xxx/usd) to the console.
 if __name__ == '__main__':
     while True:
-        api_response = dict(requests.get('https://api.bitfinex.com/v1/pubticker/' + args.arg_currency + 'usd').json())
+        api_response = dict(requests.get('https://api.bitfinex.com/v1/pubticker/' + args.arg_pair).json())
         #print(api_response)
         print(api_response.get('ask'))
         sleep(4) #We need more than a few seconds since we do the same request. Bitfinex limits are 10-90 req/m depending on site load
